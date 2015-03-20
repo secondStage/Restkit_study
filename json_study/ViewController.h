870b8a56e3d34998eff059115723189b2bb50214
作田 諒@SYS <sakuda-ryo@dmm.com>
2015-03-20 11:10:17 +0900

    Initial Commit

---
+++ json_study/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  json_study
+//
+//  Created by sakuda-ryo on 2015/03/20.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ json_study/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  json_study
+//
+//  Created by sakuda-ryo on 2015/03/20.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
